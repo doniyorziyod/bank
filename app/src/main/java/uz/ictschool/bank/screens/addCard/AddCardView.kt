92606593259057d26c2ac5b11affd304bbec6523
farrukh buriyev <120156280+farrukhburiev@users.noreply.github.com>
2024-05-05 22:04:17 +0500

    full check finished

--- app/src/main/java/uz/ictschool/bank/screens/addCard/AddCardView.kt
+++ app/src/main/java/uz/ictschool/bank/screens/addCard/AddCardView.kt
@@ -1,50 +1,174 @@
 package uz.ictschool.bank.screens.addCard
 
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material.icons.rounded.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import uz.ictschool.bank.R
+import uz.ictschool.bank.ui.theme.cardColor
+import uz.ictschool.bank.ui.theme.cardColorSecondary
+import uz.ictschool.bank.ui.theme.gray
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun AddCardView(vm: AddCardViewModel) {
-    val phoneNumber = vm.phoneNumber.observeAsState().value!!
-    var code = vm.code.observeAsState().value!!
-    Column(Modifier.fillMaxSize()) {
-        TextButton(
-            onClick = { vm.sendCode() },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.textButtonColors(
-                Color(0xFF0077F5)
+@OptIn(ExperimentalMaterial3Api::class)
+fun AddCardView(vm: AddCardViewModel, navController: NavController) {
+    val codeNumber = vm.codeNumber.observeAsState().value!!
+    val cardNumber = vm.cardNumber.observeAsState().value!!
+//    Column(Modifier.fillMaxSize()) {
+//        OutlinedTextField(
+//            value = phoneNumber,
+//            onValueChange = { vm.updatePhoneNumber(it) },
+//            modifier = Modifier.fillMaxWidth()
+//        )
+//        TextButton(
+//            onClick = { vm.sendCode() },
+//            modifier = Modifier.fillMaxWidth(),
+//            colors = ButtonDefaults.textButtonColors(
+//                Color(0xFF0077F5)
+//            )
+//        ) {
+//            Text(text = "Send code", color = Color.White)
+//        }
+//    }
+
+    Scaffold(topBar = { TopBar(vm, navController) }) {
+        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.img), contentDescription = "img",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .size(200.dp),
+//                contentScale = ContentScale.Crop
+
             )
-        ) {
-            Text(text = "Send code", color = Color.White)
-        }
 
+            Spacer(modifier = Modifier.height(70.dp))
 
+            OutlinedTextField(
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedTextColor = gray,
+                    unfocusedBorderColor = cardColorSecondary,
+                    focusedBorderColor = cardColor,
+                    focusedTextColor = Color.Black,
+                    unfocusedLabelColor = gray,
+                    focusedLabelColor = cardColor
+                ),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                trailingIcon = {
+                    IconButton(
+                        onClick = { vm.sendCode() },
 
-
-        OutlinedTextField(
-            value = code,
-            onValueChange = { vm.updateCode(it) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        TextButton(
-            onClick = { vm.checkCode(code) },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.textButtonColors(
-                Color(0xFF0077F5)
+                        ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Send,
+                            contentDescription = "Back",
+                            tint = gray
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
+                value = cardNumber,
+                label = { Text("card number") },
+                onValueChange = { if (it.length <= 16) vm.updateCardNumber(it) },
             )
-        ) {
-            Text(text = "check code", color = Color.White)
+
+            OutlinedTextField(
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedTextColor = gray,
+                    unfocusedBorderColor = cardColorSecondary,
+                    focusedBorderColor = cardColor,
+                    focusedTextColor = Color.Black,
+                    unfocusedLabelColor = gray,
+                    focusedLabelColor = cardColor
+                ),
+
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
+                value = codeNumber,
+                label = { Text("your code from telegramm") },
+                onValueChange = { if (it.length <= 6) vm.updateCodeNumber(it) },
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+            androidx.compose.material3.TextButton(
+                enabled = vm.enableDisable(), // hali ozgartiramiz
+                onClick = {
+                    Log.d("TAG", "AddCardView: check+${codeNumber}")
+                    vm.checkCode(codeNumber)
+                },
+                colors = androidx.compose.material3.ButtonDefaults.textButtonColors(
+                    containerColor = cardColor,
+                    disabledContainerColor = cardColorSecondary
+                ), modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(text = "Check code", color = Color.White, fontSize = 17.sp)
+
+            }
         }
     }
+}
+
+@Composable
+fun TopBar(vm: AddCardViewModel, navController: NavController) {
+    TopAppBar(title = {
+        androidx.compose.material3.Text(
+            text = "Add card",
+            fontWeight = FontWeight.W500
+        )
+    }, navigationIcon = {
+        IconButton(onClick = { vm.backClick(navController) }) {
+            Icon(Icons.Rounded.KeyboardArrowLeft, contentDescription = "")
+        }
+    }, backgroundColor = Color.White, contentColor = Color(0xFF121212))
 }
--- app/src/main/java/uz/ictschool/bank/screens/addCard/AddCardViewModel.kt
+++ app/src/main/java/uz/ictschool/bank/screens/addCard/AddCardViewModel.kt
@@ -2,10 +2,12 @@ package uz.ictschool.bank.screens.addCard
 
 import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import uz.ictschool.bank.MyApp
@@ -17,33 +19,52 @@ import javax.inject.Inject
 @HiltViewModel
 class AddCardViewModel @Inject constructor(private val model: AddCardModel) : ViewModel() {
     var sharedPrefHelper = SharedPrefHelper.getInstance(MyApp.context)
+    private val _codeNumber = MutableLiveData("")
+    val codeNumber: LiveData<String> = _codeNumber
 
-    private val _phoneNumber = MutableLiveData("")
-    val phoneNumber: LiveData<String> = _phoneNumber
+    private val _cardNumber = MutableLiveData("")
+    val cardNumber: LiveData<String> = _cardNumber
 
-    private val _code = MutableLiveData("")
-    val code: LiveData<String> = _code
+    private var _state = MutableLiveData(false)
+    var state: LiveData<Boolean> = _state
 
-    fun updatePhoneNumber(new: String) {
-        _phoneNumber.value = new
+    var status = ""
+
+    fun updateCardNumber(new: String) {
+        _cardNumber.value = new
     }
 
-    fun updateCode(new: String) {
-        _phoneNumber.value = new
+    fun updateCodeNumber(new: String) {
+        _codeNumber.value = new
     }
 
     fun sendCode() {
+        if (enableDisable()) {
+            viewModelScope.launch {
+                val sendCode = SendCode("+998906446151")
+                Log.d("TAG", model.sendCode(sendCode).status)
+            }
+        }
+        else Toast.makeText(MyApp.context, "card number too short", Toast.LENGTH_SHORT).show()
+    }
+
+    fun enableDisable(): Boolean {
+        if (cardNumber.value!!.length == 16) {
+            return true
+        }
+        return false
+    }
+
+    fun checkCode(code: String) {
         viewModelScope.launch {
-            val sendCode = SendCode("+998906446151")
-            Log.d("TAG", model.sendCode(sendCode).status)
-//            code = model.sendCode(sendCode).status
+            val checkCode = CheckCode("+998906446151", code)
+            status = model.checkCode(checkCode).status
+            Log.d("TAG", "checkCode: ${model.checkCode(checkCode).status}")
+//            if (state.)
         }
     }
 
-    fun checkCode(code:String){
-        viewModelScope.launch {
-            val checkCode = CheckCode(sharedPrefHelper.getUserNumber(),code)
-            Log.d("TAG", model.checkCode(checkCode).status)
-        }
+    fun backClick(navController: NavController) {
+        navController.popBackStack()
     }
 }
